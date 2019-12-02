b6ea38a254021a699b1e0bd38837a5eb52d08ed7
troybrox <danilalekseew@gmail.com>
2019-12-02 13:27:52 +0400

    UserAssignCard branch: first commit

---
+++ SimbirsfotStaging10.BLL/Interfaces/ICardService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SimbirsfotStaging10.DAL.Data;
+using SimbirsfotStaging10.DAL.Entities;
+using SimbirsfotStaging10.BLL.DTO;
+using SimbirsfotStaging10.BLL.Infrastructure;
+
+namespace SimbirsfotStaging10.BLL.Interfaces
+{
+    public interface ICardService
+    {
+        Task<OperationDetail> AddNew(CardDTO DTO);
+        Task<OperationDetail> Delete(int cardId);
+        Task<OperationDetail> Edit(int cardId, CardDTO DTO);
+        Task<(CardDTO, OperationDetail)> GetById(int cardId);
+        Task<(List<CardDTO>, OperationDetail)> GetAllFromDB();
+    }
+}
---
+++ SimbirsfotStaging10.BLL/Logger/EventType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimbirsfotStaging10.Logger
+{
+    public enum EventType
+    { 
+        RegistrationSucces=1,
+        RegistrationFail,
+        SignInSucces,
+        SignInFail,
+        Logout
+    }
+}
